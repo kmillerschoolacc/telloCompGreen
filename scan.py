4381ac0cce0edfbb0bc9c6e46a21e8f3e35538a9
KelvinBNguyen <90651084+KelvinBNguyen@users.noreply.github.com>
2023-11-08 11:38:35 -0500

    Update scan.py

--- scan.py
+++ scan.py
@@ -11,37 +11,40 @@ detector = cv2.aruco.ArucoDetector(aruco_dict, aruco_params)
 m = tello.Tello()
 m.connect()
 m.streamon()
-
-
 m.takeoff()
 
 bList = []
 
 total_distance = 0
 
+m.move_up(20)
+
 m.move_back(381)
-m.turn_left
+m.rotate_counter_clockwise(90)
 
 while total_distance < 762:
     m.send_keepalive()
     frame = m.get_frame_read().frame
     corners, ids, rejected = detector.detectMarkers(frame)
+    cv2.imshow('Frame', frame)
 
-    m.move_left(30.5)
-    total_distance += 30.5
+    m.move_right(31)
+    total_distance += 30
 
     if ids is not None:
+        cv2.aruco.drawDetectedMarkers(frame, corners, ids)
         for i in range(len(ids)):
             tag_center_x = (corners[i][0][0][0] + corners[i][0][2][0]) / 2
             frame_center_x = frame.shape[1] / 2
-            tolerance = 50
+            tolerance = 200
 
 
             if((tag_center_x - frame_center_x) < tolerance):
                 thisB = dict(id = ids[i], x = total_distance)
                 bList.append(thisB)
-print(thisB)
-    
+print(bList)
+m.streamoff()
+m.land()
 
     
     
